4ad2e0d1f442b7b18b820e99d14fffbe6977684b
Kevin <kevinpg@princeton.edu>
2015-07-24 09:06:46 +0200

    commented pickCorrelations

--- PaddleCode/lib/pickCorrelations.cpp
+++ PaddleCode/lib/pickCorrelations.cpp
@@ -8,81 +8,114 @@
 #include <assert.h>
 #include <cmath>
 /*------------------------------------------------------------------------*/
+/* For details of how to use this module, please see testPickCorrelations.cpp
+ * The basic idea is that you have an int that specifies which type of correlation
+ * you want to perform in the spatial dimension. You call pickSpatialCorr and
+ * accepts that int as an arg and retruns you a ptr to the appropriate correlation
+ * for that integer. For example, let's say you pass pickSpatialCorr the integer 1.
+ * pickSpatialCorr will return a ptr to the function gaussianSpatialCorrelation.
+ * pcikTemporalCorr works in the same way but returns a ptr to the appropriate
+ * temporal correlation function. So, which the argument 1, it returns a ptr to
+ * guassian TemporalCorr. 
+ */
+/*------------------------------------------------------------------------*/
 
-float gaussianSpatialCorr(int j, int k, double dist, float spatial_sigma){
+// gaussian correlation function in the spatial dimensions
+float gaussianSpatialCorr(int j, int k, float *ptr_to_norm, float spatial_sigma){
   return (float)(exp(-((j*j) + (k*k)) / (2 * spatial_sigma*spatial_sigma)));
 }
 
-float gaussianTemporalCorr(int t, double dist, float temporal_sigma){
-  return (float)(exp(-(t*t)/(2*temporal_sigma*temporal_sigma));
+// gaussian correlation function in the temporal dimension
+float gaussianTemporalCorr(int t, float *ptr_to_norm, float temporal_sigma){
+  return (float)(exp(-(t*t)/(2*temporal_sigma*temporal_sigma)));
 }
 
+// correlation function in the spatial dimensions
 float inverseSquareSpatialCorr(int j, int k, float *ptr_to_norm, float spatial_sigma){
   return pow( (sqrt(j*j + k*k) + 1) , -2);
 }
 
+// correlation function in the temporal dimension
 float inverseSquareTemporalCorr(int t, float *ptr_to_norm, float temporal_sigma){
   return pow( (abs(t) + 1) , -2);
 }
 
+// correlation function in the spatial dimensions
 float inverseCubeSpatialCorr(int j, int k, float *ptr_to_norm, float spatial_sigma){
   return pow( (sqrt(j*j + k*k) + 1) , -3);
 }
 
+// correlation function in the temporal dimension
 float inverseCubeTemporalCorr(int t, float *ptr_to_norm, float temporal_sigma){
   return pow( (abs(t) + 1) , -3);
 }
 
+// correlation function in the spatial dimensions
 float inverseQuarticSpatialCorr(int j, int k, float *ptr_to_norm, float spatial_sigma){
   return pow( (sqrt(j*j + k*k) + 1) , -4);
 }
 
+// correlation function in the temporal dimension
 float inverseQuarticTemporalCorr(int t, float *ptr_to_norm, float temporal_sigma){
   return pow( (abs(t) + 1) , -4);
 }
 
+// correlation function in the spatial dimensions
 float topHatSpatialCorr(int j, int k, float *ptr_to_norm, float spatial_sigma){
   return 0;
 }
 
+// correlation function in the temporal dimension
 float topHatTemporalCorr(int t, float *ptr_to_norm, float temporal_sigma){
   return 0;
 }
 
+// correlation function in the spatial dimensions
 float trueTopHatSpatialCorr(int j, int k, float *ptr_to_norm, float spatial_sigma){
   return 0;
 }
 
+// correlation function in the temporal dimension
 float trueTopHatTemporalCorr(int t, float *ptr_to_norm, float temporal_sigma){
   return 0;
 }
 
+// correlation function in the spatial dimensions
 float trueTopHatRandomSpatialCorr(int j, int k, float *ptr_to_norm, float spatial_sigma){
   return 0;
 }
 
+// correlation function in the temporal dimension
 float trueTopHatRandomTemporalCorr(int t, float *ptr_to_norm, float temporal_sigma){
   return 0;
 }
 
+// correlation function in the spatial dimensions
 float topHatLongTailSpatialCorr(int j, int k, float *ptr_to_norm, float spatial_sigma){
   return 0;
 }
 
+// correlation function in the temporal dimension
 float topHatLongTailTemporalCorr(int t, float *ptr_to_norm, float temporal_sigma){
   return 0;
 }
 
+// correlation function in the spatial dimensions
 float triangleSpatialCorr(int j, int k, float *ptr_to_norm, float spatial_sigma){
   return 0;
 }
 
+// correlation function in the temporal dimension
 float triangleTemporalCorr(int t, float *ptr_to_norm, float temporal_sigma){
   return 0;
 }
 
 /*------------------------------------------------------------------------*/
-
+/* The function pickSpatialCorr accepts an integer named typeOfSpatialCorr
+ * and returns a pointer to a function (which accepts an int, int, float*,float as parameters
+ * and returns a float)
+ * In other words, this function returns a correlation function.
+ */
 float (*pickSpatialCorr(int typeOfSpatialCorr)) (int j, int k, float *ptr_to_norm, float spatial_sigma){
   // validate parameters
   assert (typeOfSpatialCorr > 0 && typeOfSpatialCorr <= 9);
@@ -112,7 +145,11 @@ float (*pickSpatialCorr(int typeOfSpatialCorr)) (int j, int k, float *ptr_to_nor
 }
 
 /*------------------------------------------------------------------------*/
-
+/* The function pickTemporalCorr accepts an integer named typeOfTemporalCorr
+ * and returns a pointer to a function (which accepts an int, int, float*,float as parameters
+ * and returns a float)
+ * In other words, this function returns a correlation function.
+ */
 float (*pickTemporalCorr(int typeOfTemporalCorr)) (int t, float *ptr_to_norm, float temporal_sigma){
   // validate parameters
   assert (typeOfTemporalCorr > 0 && typeOfTemporalCorr <= 9);
--- PaddleCode/lib/pickCorrelations.h
+++ PaddleCode/lib/pickCorrelations.h
@@ -9,30 +9,55 @@
 #define PICK_CORRELATIONS_INCLUDED
 
 /*------------------------------------------------------------------------*/
-// The correlation functions
+/* For details of how to use this module, please see testPickCorrelations.cpp
+ * The basic idea is that you have an int that specifies which type of correlation
+ * you want to perform in the spatial dimension. You call pickSpatialCorr and
+ * accepts that int as an arg and retruns you a ptr to the appropriate correlation
+ * for that integer. For example, let's say you pass pickSpatialCorr the integer 1.
+ * pickSpatialCorr will return a ptr to the function gaussianSpatialCorrelation.
+ * pcikTemporalCorr works in the same way but returns a ptr to the appropriate
+ * temporal correlation function. So, which the argument 1, it returns a ptr to
+ * guassian TemporalCorr. 
+ */
+/*------------------------------------------------------------------------*/
+
+// The spatial correlation fuctions. pickSpatialCorr returns
+// pointers to these functions.
 float gaussianSpatialCorr(int j, int k, float *ptr_to_norm, float spatial_sigma);
-float gaussianTemporalCorr(int t, float *ptr_to_norm, float temporal_sigma);
 float inverseSquareSpatialCorr(int j, int k, float *ptr_to_norm, float spatial_sigma);
-float inverseSquareTemporalCorr(int t, float *ptr_to_norm, float temporal_sigma);
 float inverseCubeSpatialCorr(int j, int k, float *ptr_to_norm, float spatial_sigma);
-float inverseCubeTemporalCorr(int t, float *ptr_to_norm, float temporal_sigma);
 float inverseQuarticSpatialCorr(int j, int k, float *ptr_to_norm, float spatial_sigma);
-float inverseQuarticTemporalCorr(int t, float *ptr_to_norm, float temporal_sigma);
 float topHatSpatialCorr(int j, int k, float *ptr_to_norm, float spatial_sigma);
-float topHatTemporalCorr(int t, float *ptr_to_norm, float temporal_sigma);
 float trueTopHatSpatialCorr(int j, int k, float *ptr_to_norm, float spatial_sigma);
-float trueTopHatTemporalCorr(int t, float *ptr_to_norm, float temporal_sigma);
 float trueTopHatRandomSpatialCorr(int j, int k, float *ptr_to_norm, float spatial_sigma);
-float trueTopHatRandomTemporalCorr(int t, float *ptr_to_norm, float temporal_sigma);
 float topHatLongTailSpatialCorr(int j, int k, float *ptr_to_norm, float spatial_sigma);
-float topHatLongTailTemporalCorr(int t, float *ptr_to_norm, float temporal_sigma);
 float traingleSpatialCorr(int j, int k, float *ptr_to_norm, float spatial_sigma);
+
+// The temporal correlation fuctions. pickTemporalCorr returns
+// pointers to these functions.
+float gaussianTemporalCorr(int t, float *ptr_to_norm, float temporal_sigma);
+float inverseSquareTemporalCorr(int t, float *ptr_to_norm, float temporal_sigma);
+float inverseCubeTemporalCorr(int t, float *ptr_to_norm, float temporal_sigma);
+float inverseQuarticTemporalCorr(int t, float *ptr_to_norm, float temporal_sigma);
+float topHatTemporalCorr(int t, float *ptr_to_norm, float temporal_sigma);
+float trueTopHatTemporalCorr(int t, float *ptr_to_norm, float temporal_sigma);
+float trueTopHatRandomTemporalCorr(int t, float *ptr_to_norm, float temporal_sigma);
+float topHatLongTailTemporalCorr(int t, float *ptr_to_norm, float temporal_sigma);
 float triangleTemporalCorr(int t, float *ptr_to_norm, float temporal_sigma);
 
 /*------------------------------------------------------------------------*/
-// comment
+/* The function pickSpatialCorr accepts an integer named typeOfSpatialCorr
+ * and returns a pointer to a function (which accepts an int, int, float*,float as parameters
+ * and returns a float)
+ * In other words, this function returns a correlation function.
+ */
 float (*pickSpatialCorr(int typeOfSpatialCorr)) (int j, int k, float *ptr_to_norm, float spatial_sigma);
-// comment
+
+/* The function pickTemporalCorr accepts an integer named typeOfTemporalCorr
+ * and returns a pointer to a function (which accepts an int, int, float*,float as parameters
+ * and returns a float)
+ * In other words, this function returns a correlation function.
+ */
 float (*pickTemporalCorr(int typeOfTemporalCorr)) (int t, float *ptr_to_norm, float temporal_sigma);
 
 /*------------------------------------------------------------------------*/
