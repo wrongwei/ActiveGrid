3bc70f9654e1e4d0ca4e34994e5e851758a9c776
Nathan Wei <njwei23@gmail.com>
2015-08-06 18:52:52 +0200

    Fixed calculations in EdecfitClient

--- MatlabCode/EdecfitClient.m
+++ MatlabCode/EdecfitClient.m
@@ -4,8 +4,6 @@
 % Written by Nathan Wei, summer 2015
 % Dependencies: Edecfit.m
 
-% CAN WE JUST USE MASvsm AND MASvss FROM MAKEALLSTATS???
-
 clear all;
 close all;
 clc;
@@ -17,16 +15,16 @@ tic;
 fprintf('Loading data from .mat files...');
 % which workspace stats do you want to load?
 % -----------------------------------------------------------------------
-files(1) = load('statscorr_g2g1_0729_00.mat','MASvsm','u');
-files(2) = load('statscorr_g2g1_0729_01.mat','MASvsm','u');
-files(3) = load('statscorr_g2g1_0729_02.mat','MASvsm','u');
-files(4) = load('statscorr_g2g1_0729_03.mat','MASvsm','u');
-files(5) = load('statscorr_g2g1_0729_04.mat','MASvsm','u');
-files(6) = load('statscorr_g2g1_0729_05.mat','MASvsm','u');
-files(7) = load('statscorr_g2g1_0729_06.mat','MASvsm','u');
-files(8) = load('statscorr_g2g1_0729_07.mat','MASvsm','u');
-files(9) = load('statscorr_g2g1_0729_08.mat','MASvsm','u');
-files(10) = load('statscorr_g2g1_0729_09.mat','MASvsm','u');
+files(1) = load('statscorr_g2g1_0729_00.mat','MASvsm','MASvss');
+files(2) = load('statscorr_g2g1_0729_01.mat','MASvsm','MASvss');
+files(3) = load('statscorr_g2g1_0729_02.mat','MASvsm','MASvss');
+files(4) = load('statscorr_g2g1_0729_03.mat','MASvsm','MASvss');
+files(5) = load('statscorr_g2g1_0729_04.mat','MASvsm','MASvss');
+files(6) = load('statscorr_g2g1_0729_05.mat','MASvsm','MASvss');
+files(7) = load('statscorr_g2g1_0729_06.mat','MASvsm','MASvss');
+files(8) = load('statscorr_g2g1_0729_07.mat','MASvsm','MASvss');
+files(9) = load('statscorr_g2g1_0729_08.mat','MASvsm','MASvss');
+files(10) = load('statscorr_g2g1_0729_09.mat','MASvsm','MASvss');
 % -----------------------------------------------------------------------
 
 % record distances from active grid (in meters) for each data set
@@ -52,22 +50,15 @@ b0 = [1, 3, -1];
 if (length(dist) ~= length(files))
     error('Number of distances must be the same as number of files!');
 end
-fprintf(' done in %.1f seconds. \nComputing variances...', round(10*toc)/10);
-tic;
-variance = zeros(1,length(files));
-U_avg = zeros(1,length(files));
 
-for i = 1:length(files)
-    % create average velocity vector
+U_avg = zeros(length(files), 1);
+eps_avg = zeros(length(files), 1);
+
+for i = 1 : length(files)
     U_avg(i) = files(i).MASvsm;
-    % calculate variance of velocity over each data set
-    for j = 1:length(files(i).u)
-        variance(i) = variance(i) + (files(i).u(j) - U_avg(i))^2;
-    end
-    variance(i) = variance(i) / length(files(i).u); % take average over time
+    eps_avg(i) = (files(i).MASvss)^2;
 end
-
 fprintf(' done in %.1f seconds. \nSending data to Edecfit for processing.', round(10*toc)/10);
 
-result = Edecfit(dist, U_avg, variance, b0);
+result = Edecfit(dist, U_avg, eps_avg, b0);
 
