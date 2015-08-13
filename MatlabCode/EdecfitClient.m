9cd6009c1aa9669f090450c853cd95b7e2d81844
Nathan Wei <njwei23@gmail.com>
2015-08-13 14:59:26 +0200

    Improvements to EdecfitClient.m

--- MatlabCode/EdecfitClient.m
+++ MatlabCode/EdecfitClient.m
@@ -2,62 +2,44 @@
 % This program takes data from multiple .mat workspaces, and extracts
 % necessary parameters for the Edecfit.m energy decay fit function
 % Written by Nathan Wei, summer 2015
+% Requirements: any number of .mat workspaces saved in a folder, with names
+% in format '...n.mat' (where n is the test number)
 % Dependencies: Edecfit.m
 
-clear all;
-close all;
-clc;
-
-% Where is the path?-----------------------------------------------------
-pathname = fileparts('/Users/nathan/Documents/Data/data07_29_15/');
-addpath(pathname); 
-tic;
-fprintf('Loading data from .mat files...');
-% which workspace stats do you want to load?
-% -----------------------------------------------------------------------
-files(1) = load('statscorr_g2g1_0729_00.mat','MASvsm','MASvss');
-files(2) = load('statscorr_g2g1_0729_01.mat','MASvsm','MASvss');
-files(3) = load('statscorr_g2g1_0729_02.mat','MASvsm','MASvss');
-files(4) = load('statscorr_g2g1_0729_03.mat','MASvsm','MASvss');
-files(5) = load('statscorr_g2g1_0729_04.mat','MASvsm','MASvss');
-files(6) = load('statscorr_g2g1_0729_05.mat','MASvsm','MASvss');
-files(7) = load('statscorr_g2g1_0729_06.mat','MASvsm','MASvss');
-files(8) = load('statscorr_g2g1_0729_07.mat','MASvsm','MASvss');
-files(9) = load('statscorr_g2g1_0729_08.mat','MASvsm','MASvss');
-files(10) = load('statscorr_g2g1_0729_09.mat','MASvsm','MASvss');
-% -----------------------------------------------------------------------
-
-% record distances from active grid (in meters) for each data set
-% -----------------------------------------------------------------------
-dist(1) = 1.6356;
-dist(2) = 1.764634032;
-dist(3) = 1.948293331;
-dist(4) = 2.209702958;
-dist(5) = 2.581777736;
-dist(6) = 3.111366651;
-dist(7) = 3.865151737;
-dist(8) = 4.938044201;
-dist(9) = 6.465134961;
-dist(10) = 8.638704324;
-% -----------------------------------------------------------------------
-
-% enter initial parameters for curve fit (suggested: 1 3 -1)
-% -----------------------------------------------------------------------
+% -------------------------- PARAMETERS TO SET --------------------------
+pathname = fileparts('/Users/nathan/Documents/Data/data08_13_15/');
+%filebase = 'statscorr_th2.6th2_0813_0'; % files numbered 0 to tests-1
+filebase = 'statscorr_th2.6th2_0813_0';
+tests = 10; % number of data collection points along the tunnel
+% record distance from active grid to probe (in meters) for each data set
+% note: the first distance should correspond to test 0, the 2nd to test 1,
+% and so on
+dist = [1.6356; 1.764634032; 1.948293331; 2.209702958; 2.581777736; ...
+    3.111366651; 3.865151737; 4.938044201; 6.465134961; 8.638704324];
+% initial parameters for curve fit (suggested: 1 3 -1)
 b0 = [1, 3, -1];
 % -----------------------------------------------------------------------
-% end of user input requirements
 
-if (length(dist) ~= length(files))
-    error('Number of distances must be the same as number of files!');
+addpath(pathname);
+tic;
+% validation
+if (length(dist) ~= tests)
+    error('Number of distances does not match number of tests!');
 end
 
-U_avg = zeros(length(files), 1);
-eps_avg = zeros(length(files), 1);
+fprintf('Loading data from .mat files... \n');
 
-for i = 1 : length(files)
-    U_avg(i) = files(i).MASvsm;
-    eps_avg(i) = (files(i).MASvss)^2;
+U_avg = zeros(tests, 1); % array for storing mean velocities
+eps_avg = zeros(tests, 1); % array for storing mean energy (U^2)
+
+for i = 1 : tests
+    filestr = strcat(filebase, num2str(i-1), '.mat'); % find file name
+    disp(filestr);
+    vars = load(filestr,'MASvsm','MASvss');
+    U_avg(i) = vars.MASvsm; % mean flow velocity
+    eps_avg(i) = vars.MASvss^2; % rms velocity squared = average energy
 end
+
 fprintf(' done in %.1f seconds. \nSending data to Edecfit for processing.', round(10*toc)/10);
 
 result = Edecfit(dist, U_avg, eps_avg, b0);
