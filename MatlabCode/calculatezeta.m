640770578deab049bb3c19f20de8f2e0f202357e
Nathan Wei <nwei@princeton.edu>
2017-09-10 16:12:45 -0500

    Moving edits to new computer

--- MatlabCode/calculatezeta.m
+++ MatlabCode/calculatezeta.m
@@ -67,17 +67,21 @@ cutoff = 1e-12;
 h1 = (size_a(1)-1)/2+1;
 h2 = (size_a(2)-1)/2+1;
 h3 = (size_a(3)-1)/2+1;
-% corrLength = sum(C_ker(:)*dV/max(C_ker(:)))^(1/3);
-zeta2 = 0;
+corrLength = sum(C_ker(:)*dV/max(C_ker(:)))^(1/3);
 for i = 1:size_a(1)
     for j = 1:size_a(2)
         for k = 1:size_a(3)
             % calculate distance to center element
             mydist = sqrt((i-h1)^2+(j-h2)^2+(meanU*timeStep/paddled)^2*(k-h3)^2);
             % compute difference btwn. perfect corr. slope and actual slope
-            if C_ker(i,j,k) >= cutoff && mydist ~= 0
+            if C_ker(i,j,k) >= cutoff && corrLength-mydist ~= 0
                 vol = vol + dV;
-                zeta = zeta + dV*(C_ref(i,j,k)-C_ker(i,j,k))/(mydist*((1/(1-C_ref(i,j,k)))-1));
+                % zeta = zeta + dV*(C_ref(i,j,k)-C_ker(i,j,k))/(mydist*((1/(1-C_ref(i,j,k)))-1)); % slope differences
+                % zeta = zeta + dV*(C_ker(i,j,k))/(mydist*((1/(1-C_ref(i,j,k)))-1)); % just slope
+                zeta = zeta + dV*(C_ker(i,j,k))/abs(mydist-corrLength); % l_alpha instead of rng - not bad w/ or w/o abs
+                % GO BACK AND TRY USING L_ALPHA INSTEAD OF RNG BUT KEEP ALL
+                % SLOPES POSITIVE. TRY THIS AND TRY WITH ACTUAL SLOPE AT
+                % L_ALPHA (more physical but will probably fail)
             end
         end
     end
