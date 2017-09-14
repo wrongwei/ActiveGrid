b8008c8e605734df70adae5738789111675b3540
Nathan Wei <njwei23@gmail.com>
2017-09-13 22:01:03 -0400

    Refinements to formatfigs

--- MatlabCode/formatfigs.m
+++ MatlabCode/formatfigs.m
@@ -11,29 +11,38 @@
 
 %% ---------------------------- USER INPUTS ---------------------------- %%
 
-fig_name    = 'corrf_lt3.9lt3_lt6.5lt5.fig'; % figure to modify
-eps_name    = 'Fig747.eps'; % name to save .eps version under
-fig_dir     = '../Figures/'; % relative or absolute path, where .fig lives
+fig_name    = 'all correlations paper.fig'; % figure to modify
+eps_name    = 'Fig9.eps'; % name to save .eps version under
+fig_dir     = '../Figures/ExpFluids'; % relative or absolute path, where .fig lives
 save_fig    = 1; % export as .eps and save revised .fig if 1
 
 %% ---------------------------- FORMATTING ----------------------------- %%
 
 close all; % make sure nothing else is open before starting
+og_dir = pwd();
 
 f1 = open(fullfile(fig_dir, fig_name)); % find and open figure
+hax = gca;
 
 % Set parameters
+set(f1, 'WindowStyle', 'normal');
 set(f1,'color','w'); % figure must be in black and white
 set(findall(f1,'-property','fontsize'),'fontsize',20); % must be legible
 set(findall(f1,'-property','markersize'),'markersize',20);
 set(findall(f1,'-property','linewidth'),'linewidth',2); % > 0.1 mm wide
 set(findall(f1,'-property','interpreter'),'interpreter','latex');
-f1_size = get(f1,'position');
-f1_size(3:4) = [800 800]; % set new dimensions of figure
-set(f1,'position',f1_size);
+ttl = get(hax, 'Title');
+set(ttl, 'fontsize', 20, 'fontweight', 'bold');
+leg = get(hax, 'Legend');
+set(leg, 'fontsize', 15);
+f1 = gcf;
+f1_size = [1, 1, 800, 800]; % set new dimensions of figure
+set(f1,'Position',f1_size);
+set(f1,'PaperSize',f1_size(3:4));
 
 if save_fig
     cd(fig_dir);
     saveas(gcf, fig_name); % save revised figure under same name
     print(eps_name, '-depsc', '-tiff'); % export as eps
+    cd(og_dir);
 end
